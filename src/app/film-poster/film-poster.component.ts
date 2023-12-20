b841956a068ead018731612ef7f03c5ab2b1bb20
Georges <georgesghassankhalil@gmail.com>
2023-12-20 21:50:13 +0200

    reset

--- src/app/film-poster/film-poster.component.ts
+++ src/app/film-poster/film-poster.component.ts
@@ -10,38 +10,19 @@ import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 export class FilmPosterComponent implements OnInit {
   @Input() film: any;
   @Output() posterClicked = new EventEmitter<number>();
-  isFavorite: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
-    const favorites = JSON.parse(localStorage.getItem('favorites') || '{}');  // {} is default value, if no favorites in local storage
-    this.isFavorite = !!favorites[this.film.id];  // !! converts to boolean
-  }
 
-  toggleFavorite(): void {
-    this.isFavorite = !this.isFavorite;   // Toggle favorite status
-    if (this.isFavorite) {                // Add or remove from favorites
-      this.addToFavorites();
-    } else {
-      this.removeFromFavorites();
-    }
-  }
-
-  addToFavorites() {
-    const favorites = JSON.parse(localStorage.getItem('favorites') || '{}');    // {} is default value, if no favorites in local storage
-    favorites[this.film.id] = this.film;                                        // Add to favorites                
-    localStorage.setItem('favorites', JSON.stringify(favorites));               // Save to local storage
-  }
-
-  removeFromFavorites() {
-    const favorites = JSON.parse(localStorage.getItem('favorites') || '{}');    // {} is default value, if no favorites in local storage
-    delete favorites[this.film.id];                                             // Remove from favorites
-    localStorage.setItem('favorites', JSON.stringify(favorites));               // Save to local storage         
   }
 
   onPosterClick(): void {
     this.posterClicked.emit(this.film.id);
+  }
+
+  toggleFavorite(): void{
+    this.film.favorite = !this.film.favorite;
   }
 
 }
--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -13,7 +13,6 @@ export class MovieService {
       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0YmJjZGIyZWUxM2NjY2NkMzVlNjUwOTAxNTY0ZjJjYSIsInN1YiI6IjY1NTA5YjVhZDQ2NTM3MDBhYjk2NDBkNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.mV-OK2fGrRYdzTn916V2eABzTIIIx-EEhNwvGRRSapE'
     }
   };
-  favorites: { [id: number]: boolean } = {};  // Dictionary of film IDs to favorite status
 
   constructor(private http: HttpClient) { }
 
@@ -23,35 +22,6 @@ export class MovieService {
 
   getMoviesByGenre(genreId: number, page: number = 1): Observable<any> {
     return this.http.get(`${this.apiUrl}/discover/movie?include_adult=false&include_video=false&language=en-US&page=${page}&sort_by=popularity.desc&with_genres=${genreId}`, this.options);
-  }
-
-  addToFavorites(movieId: number): void {
-    this.favorites[movieId] = true;
-    this.saveFavoritesToLocalStorage();
-  }
-
-  removeFromFavorites(movieId: number): void {
-    delete this.favorites[movieId];
-    this.saveFavoritesToLocalStorage();
-  }
-
-  getFavorites(): { [id: number]: boolean } {
-    return this.favorites;
-  }
-
-  isFavorite(movieId: number): boolean {
-    return this.favorites[movieId] === true;
-  }
-
-  private saveFavoritesToLocalStorage(): void {
-    localStorage.setItem('favorites', JSON.stringify(this.favorites));
-  }
-
-  private loadFavoritesFromLocalStorage(): void {
-    const favorites = localStorage.getItem('favorites');
-    if (favorites) {
-      this.favorites = JSON.parse(favorites);
-    }
   }
   
   getMovieDetails(movieId: number): Observable<any> {
